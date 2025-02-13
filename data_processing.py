3e8007c54b8db2081a102724c3af2fd1705087e9
HoiTKN <hoitkn@msc.masangroup.com>
2025-02-13 12:48:23 +0700

    data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -91,23 +91,16 @@ def prepare_data():
     Prepare data for analysis
     """
     try:
-        # Get spreadsheet ID from secrets
-        spreadsheet_id = st.secrets["global"]["spreadsheet_id"]
+        # Debug prints
+        print("Available secrets keys:", st.secrets.keys())
         
-        # Create credentials dict from secrets
-        credentials_dict = {
-            "type": st.secrets["gcp_service_account"]["type"],
-            "project_id": st.secrets["gcp_service_account"]["project_id"],
-            "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-            "private_key": st.secrets["gcp_service_account"]["private_key"],
-            "client_email": st.secrets["gcp_service_account"]["client_email"],
-            "client_id": st.secrets["gcp_service_account"]["client_id"],
-            "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-            "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-            "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-            "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-            "universe_domain": st.secrets["gcp_service_account"]["universe_domain"]
-        }
+        # Get spreadsheet ID from secrets
+        spreadsheet_id = st.secrets["SPREADSHEET_ID"]
+        print("Spreadsheet ID:", spreadsheet_id)
+        
+        # Try to access credentials
+        credentials_str = st.secrets["GOOGLE_CREDENTIALS"]
+        print("Credentials found")
         
         # Get data from Google Sheets
         df = get_google_sheet_data(spreadsheet_id)
